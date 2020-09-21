e21a8af35879c49e70ee0b81991d593c1ef7123e
Thorsten Lorenz <thlorenz@gmx.de>
2020-09-20 18:09:52 -0600

    init: including csv file in binary

--- bin/init_data.rs
+++ bin/init_data.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File};
+use std::error::Error;
 
 use nf_rated::data::{CsvRow, Db};
 
@@ -6,9 +6,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let db = Db::new()?;
     db.create_table()?;
 
-    let file_path = "resources/data/netflix_titles.csv";
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let csv = include_str!("../resources/data/netflix_titles.csv");
+    let mut rdr = csv::Reader::from_reader(csv.as_bytes());
     for result in rdr.records() {
         let row: CsvRow = result?.into();
         db.upsert_row(&row.into())?;
